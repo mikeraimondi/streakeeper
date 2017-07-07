dff245c0c287738a9692e86b4b7436944c1ae032
Michael Raimondi <mike@mikeraimondi.com>
2017-07-06 22:13:27 -0400

    test deploy

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ async function takeScreenshot(page, file) {
   await writeFileAsync(file, buffer, "base64");
 }
 
-async function doIt() {
+const run = async () => {
   const chrome = await launchChrome({
     flags: [
       "--window-size=1280x1696",
@@ -23,31 +23,26 @@ async function doIt() {
     ],
   });
   const client = await CDP();
-  try {
-    const { DOM, Network, Page } = client;
-    await Promise.all[Page.enable(), DOM.enable(), Network.enable()];
+  const { DOM, Page, Network } = client;
+  await Page.enable();
+  await DOM.enable();
+  await Network.enable();
 
-    await Page.navigate({ url: "https://duolingo.com" });
-    await Page.loadEventFired();
-    // await new Promise((resolve) => setTimeout(resolve, 2000)); // wait for 2s for JS to finish
-    await takeScreenshot(Page, "test.png");
-  } finally {
-    if (client) {
+  await Page.navigate({ url: "https://duolingo.com" });
+  Page.loadEventFired(async () => {
+    try {
+      const { root: { nodeId: documentNodeId } } = await DOM.getDocument();
+      const html = await DOM.getOuterHTML({ nodeId: documentNodeId });
+      console.log(html);
+    } finally {
       await client.close();
+      await chrome.kill();
     }
-    await chrome.kill();
-  }
-}
+  });
+};
 
-export const hello = (event, context, callback) => {
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: "Go Serverless v1.0! Your function executed successfully!",
-  //     input: event,
-  //   }),
-  // };
-  doIt().then(() => {
+export const handler = (event, context, callback) => {
+  run().then(() => {
     callback(null);
   }).catch((err) => {
     callback(err);
